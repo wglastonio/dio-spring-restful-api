50735a0f4e503254961e34c67db223dc12e41ba1
wglastonio <wglastonio@gmail.com>
2024-05-16 08:44:34 -0300

    First commit

---
+++ src/test/java/com/dio/restfulApi/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.restfulApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
